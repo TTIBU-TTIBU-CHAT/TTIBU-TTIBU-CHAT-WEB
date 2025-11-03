53218e8bc2818346888e668e92dd0b430a854176
Hodu-moon <dudgh614@gmail.com>
2025-11-03 13:16:04 +0900

    feature: backend

---
+++ backend/src/test/java/io/ssafy/p/k13c103/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.k13c103.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
